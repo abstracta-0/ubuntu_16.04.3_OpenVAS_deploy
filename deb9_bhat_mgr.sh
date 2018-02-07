5f28a8ab9acb5e891c8a823e9ad3c01f58dd3c9d
abstracta-0 <36171307+abstracta-0@users.noreply.github.com>
2018-02-07 14:03:53 -0600

    Update deb9_bhat_mgr.sh

--- deb9_bhat_mgr.sh
+++ deb9_bhat_mgr.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 # insure that the system is up to date
-apt update && apt upgrade -y && apt dist-upgrade -y
+apt update && apt upgrade && apt dist-upgrade
 
 apt install -y build-essential cmake bison flex libpcap-dev pkg-config libglib2.0-dev libgpgme11-dev uuid-dev sqlfairy xmltoman doxygen libssh-dev libksba-dev libldap2-dev libsqlite3-dev libmicrohttpd-dev libxml2-dev libxslt1-dev xsltproc clang rsync rpm nsis alien sqlite3 libhiredis-dev libgcrypt11-dev libgnutls28-dev redis-server texlive-latex-base texlive-latex-recommended linux-headers-$(uname -r) python python-pip mingw-w64 heimdal-multidev libpopt-dev libglib2.0-dev gnutls-bin certbot nmap ufw
 
