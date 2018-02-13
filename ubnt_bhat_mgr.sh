98c07dcb5d8b375b61b2684be5eb82f43878e14d
abstracta-0 <36171307+abstracta-0@users.noreply.github.com>
2018-02-13 11:20:13 -0600

    Update ubnt_bhat_mgr.sh

--- ubnt_bhat_mgr.sh
+++ ubnt_bhat_mgr.sh
@@ -117,7 +117,9 @@ cp /etc/OpenVAS/ubuntu_16.04.3_OpenVAS_deploy/openvas-manager.service /etc/syste
 cp /etc/OpenVAS/ubuntu_16.04.3_OpenVAS_deploy/openvas-scanner.service /etc/systemd/system/openvas-scanner.service
 cp /etc/OpenVAS/ubuntu_16.04.3_OpenVAS_deploy/greenbone-security-assistant.service /etc/systemd/system/greenbone-security-assistant.service
 
-systemctl enable openvas-manager.service && systemctl enable openvas-scanner.service && systemctl enable greenbone-security-assistant.service
+systemctl enable openvas-manager.service
+systemctl enable openvas-scanner.service
+systemctl enable greenbone-security-assistant.service
 
 cp /etc/systemd/system/redis.service /etc/systemd/system/redis.service.bak
 sed -i 's+PrivateTmp=yes+PrivateTmp=no+' /etc/systemd/system/redis.service
