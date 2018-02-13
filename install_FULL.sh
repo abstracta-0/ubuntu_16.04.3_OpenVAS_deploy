d0db1b2bd029645f5d16fde914a097c294741ef7
abstracta-0 <36171307+abstracta-0@users.noreply.github.com>
2018-02-12 18:45:58 -0600

    Update install_FULL.sh

--- install_FULL.sh
+++ install_FULL.sh
@@ -9,10 +9,10 @@ cd $(echo $path2 | tr -d '\r')
 apt-get update && apt-get dist-upgrade -y && apt-get install git
 
 
-git clone https://github.com/abstracta-0/deb9_OpenVAS_deploy.git
+git clone https://github.com/abstracta-0/ubuntu_16.04.3_OpenVAS_deploy.git
 
 cd deb9_OpenVAS_deploy
 
 chmod +x *
 
-/bin/bash /etc/OpenVAS/deb9_OpenVAS_deploy/deb9_bhat_mgr.sh |& tee install.log
+/bin/bash /etc/OpenVAS/ubuntu_16.04.3_OpenVAS_deploy/ubnt_bhat_mgr.sh |& tee install.log
