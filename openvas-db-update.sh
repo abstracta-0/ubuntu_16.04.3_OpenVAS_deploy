218851b68cfbbc21637f3b1d620abc34a2dc0a02
abstracta-0 <36171307+abstracta-0@users.noreply.github.com>
2018-02-21 09:50:55 -0600

    added service check

--- openvas-db-update.sh
+++ openvas-db-update.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 echo "Updating OpenVAS Databases"
+systemctl is-active --quiet openvas-scanner.service
+
+status=$(echo $?)
+
+while [ $status != 0 ];do
+	systemctl start openvas-scanner
+	status=$(echo $?)
+done
+
+systemctl is-active --quiet openvas-manager.service
+status=$(echo $?)
+
+while [ $status != 0 ];do
+	systemctl start openvas-manager
+	status=$(echo $?)
+done
+
+systemctl is-active --quiet greenbone-security-assistant.service
+status=$(echo $?)
+
+while [ $status != 0 ];do
+	systemctl start greenbone-security-assistant
+	status=$(echo $?)
+done
+
 greenbone-nvt-sync
 greenbone-scapdata-sync
 greenbone-certdata-sync
+
+openvasmd --update
